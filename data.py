abf5739387a466cfdd41152661b6ad3aea4506d1
gamesbysl1m <47889557+gamesbysl1m@users.noreply.github.com>
2019-03-06 20:20:18 +0300

    Add files via upload

--- data.py
+++ data.py
@@ -9,14 +9,8 @@ driver = webdriver.Chrome('data\chromedriver.exe') #driver location
 def random_logpassw(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def random_names ():
-    return random.choice(list(open("data\listnames.txt", "r")))
-
-def random_surnames ():
-    return random.choice(list(open("data\listsurnames.txt", "r")))
-
-def random_about ():
-    return random.choice(list(open("data\listabout.txt", "r")))
+def random_list (list_location = " "):
+    return random.choice(list(open(list_location, "r")))
 
 def connection_error ():
     print('Не удается подключиться')
