3b149626bb0503b11047e91dd2d054a487e7e3ac
gamesbysl1m <47889557+gamesbysl1m@users.noreply.github.com>
2019-03-06 12:20:25 +0300

    Add files via upload

--- autoreg.py
+++ autoreg.py
@@ -1,21 +1,22 @@
 from data import *
 
 i = 0
-n = 3 #Number of registrations
+n = 1 #Number of registrations
 driver = webdriver.Chrome('data\chromedriver.exe')
 logpass = open("log_pass.txt", "a")
 print('Загружено имен:', len(open("data\listnames.txt", "r").readlines()))
 print('Загружено фамилий:', len(open("data\listsurnames.txt", "r").readlines()))
+print('Загружено "О себе":', len(open("data\listabout.txt", "r").readlines()))
 print('-------------------------\n')
 while i < n:
     driver.get("http://84.201.143.169/register")
-    password = random_logpassw(7, string.ascii_uppercase)
+    password = random_logpassw(7, string.ascii_uppercase + string.ascii_lowercase + string.digits)
     login = random_logpassw(6, string.ascii_lowercase)
     driver.find_element_by_id("name").send_keys(random_names())
     driver.find_element_by_id("surname").send_keys(random_surnames())
     driver.find_element_by_id("email").send_keys(random_logpassw(10, string.ascii_uppercase)+'@gmail.com')
     driver.find_element_by_id("date").send_keys("20.12.2077")
-    driver.find_element_by_id("about_me").send_keys("НЕ БЕЙТЕ ПАЛКОЙ")
+    driver.find_element_by_id("about_me").send_keys(random_about())
     driver.find_element_by_id("username").send_keys(login)
     driver.find_element_by_id("password").send_keys(password)
     driver.find_element_by_id("retype_password").send_keys(password)
@@ -27,4 +28,5 @@ while i < n:
     i = i + 1
     print("Кол-во регистраций: ", i)
 
+logpass.close()
 driver.close()
--- data.py
+++ data.py
@@ -3,11 +3,14 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def random_logpassw(size=6, chars=string.ascii_uppercase + string.digits):
+def random_logpassw(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def random_names ():
-    return random.sample(list(open("data\listnames.txt", "r")), 1)
+    return random.choice(list(open("data\listnames.txt", "r")))
 
 def random_surnames ():
-    return random.sample(list(open("data\listsurnames.txt", "r")), 1)
+    return random.choice(list(open("data\listsurnames.txt", "r")))
+
+def random_about ():
+    return random.choice(list(open("data\listabout.txt", "r")))
