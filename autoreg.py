85d2e6b39a0bc9dcf1d9cec105090c418ddea2e8
gamesbysl1m <47889557+gamesbysl1m@users.noreply.github.com>
2019-03-06 15:43:33 +0300

    Add files via upload

--- autoreg.py
+++ autoreg.py
@@ -2,19 +2,21 @@ from data import *
 
 i = 0
 n = 1 #Number of registrations
-driver = webdriver.Chrome('data\chromedriver.exe')
 logpass = open("log_pass.txt", "a")
 print('Загружено имен:', len(open("data\listnames.txt", "r").readlines()))
 print('Загружено фамилий:', len(open("data\listsurnames.txt", "r").readlines()))
 print('Загружено "О себе":', len(open("data\listabout.txt", "r").readlines()))
 print('-------------------------\n')
+
 while i < n:
     driver.get("http://84.201.143.169/register")
+    if driver.title != 'PuralNet':
+        connection_error()
     password = random_logpassw(7, string.ascii_uppercase + string.ascii_lowercase + string.digits)
     login = random_logpassw(6, string.ascii_lowercase)
     driver.find_element_by_id("name").send_keys(random_names())
     driver.find_element_by_id("surname").send_keys(random_surnames())
-    driver.find_element_by_id("email").send_keys(random_logpassw(10, string.ascii_uppercase)+'@gmail.com')
+    driver.find_element_by_id("email").send_keys(random_logpassw(10, string.ascii_uppercase) + '@gmail.com')
     driver.find_element_by_id("date").send_keys("20.12.2077")
     driver.find_element_by_id("about_me").send_keys(random_about())
     driver.find_element_by_id("username").send_keys(login)
@@ -22,6 +24,8 @@ while i < n:
     driver.find_element_by_id("retype_password").send_keys(password)
     driver.find_element_by_id("submit").send_keys(Keys.ENTER)
     driver.get("http://84.201.143.169/logout")
+    if driver.title != 'PuralNet':
+        connection_error()
     print('USERNAME: ' + login, file=logpass)
     print('PASSWORD: ' + password, file=logpass)
     print(' ', file=logpass)
--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 import string
 import random
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('data\chromedriver.exe') #driver location
 
 def random_logpassw(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -14,3 +17,9 @@ def random_surnames ():
 
 def random_about ():
     return random.choice(list(open("data\listabout.txt", "r")))
+
+def connection_error ():
+    print('Не удается подключиться')
+    time.sleep(5)
+    driver.close()
+    exit()
