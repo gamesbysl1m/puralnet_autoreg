611ca0f27132ef0d1a0e4c2450e3cb7546d56f57
gamesbysl1m <47889557+gamesbysl1m@users.noreply.github.com>
2019-03-12 00:48:27 +0300

    Add files via upload

--- autoreg.py
+++ autoreg.py
@@ -1,36 +1,42 @@
 from data import *
 
-i = 0
-n = 1 #Number of registrations
+current_registrations_count = 0
+registrations_count = 10
 logpass = open("log_pass.txt", "a")
-print('Загружено имен:', len(open("data\listnames.txt", "r").readlines()))
-print('Загружено фамилий:', len(open("data\listsurnames.txt", "r").readlines()))
-print('Загружено "О себе":', len(open("data\listabout.txt", "r").readlines()))
+print('Names loaded:', len(open("data\listnames.txt", "r").readlines()))
+print('SurNames loaded:', len(open("data\listsurnames.txt", "r").readlines()))
+print('Uploaded "about me":', len(open("data\listabout.txt", "r").readlines()))
 print('-------------------------\n')
 
-while i < n:
-    driver.get("http://84.201.143.169/register")
-    if driver.title != 'PuralNet':
-        connection_error()
-    password = random_logpassw(7, string.ascii_uppercase + string.ascii_lowercase + string.digits)
-    login = random_logpassw(6, string.ascii_lowercase)
-    driver.find_element_by_id("name").send_keys(random_list("data\listnames.txt"))
-    driver.find_element_by_id("surname").send_keys(random_list("data\listsurnames.txt"))
-    driver.find_element_by_id("email").send_keys(random_logpassw(10, string.ascii_uppercase) + '@gmail.com')
-    driver.find_element_by_id("date").send_keys("20.12.2077")
-    driver.find_element_by_id("about_me").send_keys(random_list("data\listabout.txt"))
-    driver.find_element_by_id("username").send_keys(login)
-    driver.find_element_by_id("password").send_keys(password)
-    driver.find_element_by_id("retype_password").send_keys(password)
-    driver.find_element_by_id("submit").send_keys(Keys.ENTER)
-    driver.get("http://84.201.143.169/logout")
-    if driver.title != 'PuralNet':
-        connection_error()
-    print('USERNAME: ' + login, file=logpass)
-    print('PASSWORD: ' + password, file=logpass)
-    print(' ', file=logpass)
-    i = i + 1
-    print("Кол-во регистраций: ", i)
+try:
+    while current_registrations_count < registrations_count:
+        driver.get("http://84.201.143.169/register")
+        if driver.title != 'PuralNet':
+            connection_error()
+        password = random_logpassw(7, string.ascii_uppercase + string.ascii_lowercase + string.digits)
+        login = random_logpassw(6, string.ascii_lowercase)
+        driver.find_element_by_id("name").send_keys(random_list("data\listnames.txt"))
+        driver.find_element_by_id("surname").send_keys(random_list("data\listsurnames.txt"))
+        driver.find_element_by_id("email").send_keys(random_logpassw(10, string.ascii_uppercase) + '@gmail.com')
+        driver.find_element_by_id("date").send_keys(random_date())
+        driver.find_element_by_id("about_me").send_keys(random_list("data\listabout.txt"))
+        driver.find_element_by_id("username").send_keys(login)
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_id("retype_password").send_keys(password)
+        driver.find_element_by_id("submit").send_keys(Keys.ENTER)
+        current_registrations_count = current_registrations_count + 1
+        print("Number of registrations: ", current_registrations_count)
+        user_activity()
+        driver.get("http://84.201.143.169/logout")
+        if driver.title != 'PuralNet':
+            connection_error()
+        print('USERNAME: ' + login, file=logpass)
+        print('PASSWORD: ' + password, file=logpass)
+        print(' ', file=logpass)
+        print('-------------------------')
+
+except Exception:
+        pass
 
 logpass.close()
 driver.close()
--- data.py
+++ data.py
@@ -12,8 +12,35 @@ def random_logpassw(size=6, chars=string.ascii_uppercase + string.ascii_lowercas
 def random_list (list_location = " "):
     return random.choice(list(open(list_location, "r")))
 
+def random_date():
+    return (str(random.randint(1, 29))+'.'+str(random.randint(1, 12))+'.'+str(random.randint(1980, 2007)))
+
+def user_activity():
+    a = 1
+    followers_count = 0
+    followers_count_max = 25
+    url = driver.current_url
+    users_count = url.replace("http://84.201.143.169/user/id", "")
+    counter = random.randint(10, int(users_count)-80)
+    print('Number of follows needed: ', counter)
+    print('Max follows: ', followers_count_max)
+    while a < counter and followers_count < followers_count_max:
+        driver.get("http://84.201.143.169/user/id" + str(random.randint(0, int(users_count))))
+        try:
+            driver.find_element_by_class_name('btn-success').send_keys(Keys.ENTER)
+            followers_count = followers_count + 1
+            a = a + 1
+            print(followers_count)
+            time.sleep(1)
+        except Exception:
+            followers_count = followers_count + 1
+            a = a + 1
+            print('Subscription error ', followers_count)
+            pass
+
+
 def connection_error ():
-    print('Не удается подключиться')
+    print('Unable to connect, shutdown')
     time.sleep(5)
     driver.close()
     exit()
